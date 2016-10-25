e2b43c7e55e2767800615b09dde9452a903ea7c2
Sofiane Battou <sofiane.battou@qlik.com>
2016-10-25 10:49:07 -0400

    First commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simple.CredentialManager;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Credential.LoadAll().Where(c => c.Target.Contains("Qlik_")).ToList().ForEach(c => c.Delete());
+            Console.WriteLine("The deletion of the Qlik targets is complete");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ test/CredentialChecker/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Simple.CredentialManager;
+
+namespace CredentialChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WriteSingleCredential();
+
+            WriteAllCredentials();
+
+            Console.ReadLine();
+        }
+
+        private static void WriteSingleCredential()
+        {
+            var credential = new Credential {Target = "Test"};
+            credential.Load();
+
+            Console.WriteLine("User name: {0}, Password: {1}", credential.Username, credential.Password);
+        }
+
+        private static void WriteAllCredentials()
+        {
+            var loadAll = Credential.LoadAll();
+
+            foreach (var credential1 in loadAll)
+            {
+                Console.WriteLine(credential1);
+            }
+        }
+    }
+}
